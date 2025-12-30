336f1fc0c8fd8afc0156821b249c848d671973b9
youssefahmedyr <youssefahmedyryr123@gmail.com>
2025-12-31 00:22:09 +0200

    Refactor brands API functions (clean return types)

--- app/_api/brands.ts
+++ app/_api/brands.ts
@@ -1,5 +1,8 @@
 import Api from './api'
 
+/**
+ * Brand model
+ */
 export type Brand = {
   _id: string
   name: string
@@ -7,21 +10,32 @@ export type Brand = {
   image: string
 }
 
+/**
+ * API response for brands list
+ * API returns: { data: Brand[] }
+ */
 type BrandsResponse = {
   data: Brand[]
 }
 
-export async function getBrands(): Promise<BrandsResponse> {
+/**
+ * Get all brands
+ */
+export async function getBrands(): Promise<Brand[]> {
   const api = Api()
 
   try {
     const res = await api.get<BrandsResponse>('/brands')
-    return res.data
+    return res.data.data
   } catch (error) {
     throw error
   }
 }
 
+/**
+ * Get single brand by id
+ * API returns: { data: Brand }
+ */
 export async function getBrandById(id: string): Promise<Brand> {
   const api = Api()
 
